97415f652a393eb89b0542d9550e1de52726f72c
LakshmiPriya-22 <lakshmipriya6177@gmail.com>
2025-12-27 12:01:52 +0530

    Add estimated wait calculation logic

--- appointments/utils.py
+++ appointments/utils.py
@@ -1,9 +1,30 @@
+from datetime import date
+from django.db.models import Max
 from .models import Appointment
 
-def generate_token():
-    last = Appointment.objects.order_by('-id').first()
-    if last:
-        number = int(last.token[1:]) + 1
-    else:
-        number = 1
-    return f"A{number}"
+AVG_SERVICE_TIME = 5  # minutes per appointment
+
+
+def generate_token(today=None):
+    if today is None:
+        today = date.today()
+
+    last_token = Appointment.objects.filter(
+        date=today
+    ).aggregate(max_token=Max('token'))['max_token']
+
+    if last_token is None:
+        return 1
+
+    return last_token + 1
+
+
+def calculate_estimated_wait():
+    today = date.today()
+
+    waiting_count = Appointment.objects.filter(
+        date=today,
+        status='waiting'
+    ).count()
+
+    return waiting_count * AVG_SERVICE_TIME
